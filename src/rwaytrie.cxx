38a701355d9fef70066564a6c1448307c195610c
Luke Warkentin <theloneneutrino@gmail.com>
2025-07-19 17:33:17 -0700

    removed unnecesary file

--- src/rwaytrie.cxx
+++ src/rwaytrie.cxx
@@ -1,8 +1,21 @@
-#include "rwaytrie.h"
+#include "rwaytrie.hxx"
 
-Node::Node()
+Node::Node(int value)
 {
-   
+  this->value = value;
+
+  for (int i = 0; i < ALPHABET_SIZE; i++)
+    next[i] = nullptr;
 }
 
+Node::~Node()
+{
+  for (int i = 0; i < ALPHABET_SIZE; i++)
+    if(next[i] != nullptr)
+      delete next[i];
+}
 
+RWayTrie::RWayTrie()
+{
+
+}
