dd2151b7882ae4cde0bd993e43488eb2fa26843c
Luke Warkentin <theloneneutrino@gmail.com>
2025-07-22 10:16:05 -0700

    testing ansi colors

--- src/test1.cxx
+++ src/test1.cxx
@@ -1,5 +1,6 @@
 #include <print>
 #include "rwaytrie.hxx"
+#include "ansi.hxx"
 
 int main()
 {
@@ -8,9 +9,9 @@ int main()
 
   trieObj.insert(keys[0], 1);
   if (trieObj.contains(keys[0]))
-    std::println("Contains {}.", keys[0]);
+    std::println("Contains {}{}{}.", ANSI::GREEN, keys[0], ANSI::RESET);
   else
-    std::println("Doesn't contain {}.", keys[0]);
+    std::println("Doesn't contain {}{}{}.", ANSI::RED, keys[0], ANSI::RESET);
 
   if (trieObj.contains(keys[1]))
     std::println("Contains {}.", keys[1]);
